6ea17240789e5875c19443010bd78e8516431bcc
moykul <moykul@live.com>
2023-02-03 16:24:24 +0000

    Create README.md
    This is a drawing app created in p5

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 function setup() {
   createCanvas(400, 400);
   background(120);
+  noStroke()
 }
 
 function draw() {
-  
+  fill(200, 25, 25)
+  circle(300, 40, 25);
 
 
 }
 
 function mouseDragged(){
   noStroke()
-  fill(100, 180, 80)
+  fill(20, 180, 120)
   circle(mouseX, mouseY, 20)
 
 }
