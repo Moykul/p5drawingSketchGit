6b3b6b8fcdcafb1e070335cdc1ac0e145a5acdd3
Moykul <moykul@live.com>
2023-02-03 16:21:29 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  background(120);
+}
+
+function draw() {
+  
+
+
+}
+
+function mouseDragged(){
+  noStroke()
+  fill(100, 180, 80)
+  circle(mouseX, mouseY, 20)
+
+}
