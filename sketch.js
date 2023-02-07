1d4c13b22bde9ce8d1af8b6003b59b0208924eac
Moykul <moykul@live.com>
2023-02-07 06:36:08 +0000

    Master slider added to change brush size

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
+let sldr;
 function setup() {
   createCanvas(400, 400);
   background(120);
-  noStroke()
+  noStroke();
+  sldr = createSlider(10, 80, 30);
+  sldr.position(width/2, height + 80);
 }
 
 function draw() {
-  fill(200, 25, 25)
+  fill(200, 25, 25);
   circle(300, 40, 25);
 
 
 }
 
 function mouseDragged(){
-  noStroke()
-  fill(180, 180, 60)
-  circle(mouseX, mouseY, 20)
+  noStroke();
+  let br = sldr.value();
+  fill(180, 180, 60);
+  circle(mouseX, mouseY, br);
 
 }
